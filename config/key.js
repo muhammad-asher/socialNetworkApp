fc3da09ad02da906f5427baeecba62792ca6b17d
muhammad-asher <kmuhammadasher0308@gmail.com>
2021-03-19 23:27:46 +0500

    Firebase Config

---
+++ config/key.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+	apiKey: "AIzaSyDn5M4dLtcgyGLO17iloLBqZFtz1J7CxKA",
+	authDomain: "social-network-app-45b3d.firebaseapp.com",
+	projectId: "social-network-app-45b3d",
+	storageBucket: "social-network-app-45b3d.appspot.com",
+	messagingSenderId: "521524199633",
+	appId: "1:521524199633:web:dc2f9ce40b238440ad05f0",
+	measurementId: "G-8H0TV76RR5",
+};
