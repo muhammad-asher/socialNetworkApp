c82358cdb9a1932f98de0f1d433b4bfb7c133c35
muhammad-asher <kmuhammadasher0308@gmail.com>
2021-03-19 23:28:18 +0500

    Firebase Authentication with LoginScreen

--- App.js
+++ App.js
@@ -1,21 +1,37 @@
+import React, { useState } from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import React from "react";
-import { StyleSheet, View,} from "react-native";
+import { StyleSheet, View, SafeAreaView } from "react-native";
 import BottomTabs from "./app/navigation/BottomTabs";
+import AuthStack from "./app/navigation/AuthStack";
+import firebase from "firebase";
+import { firebaseConfig } from "./config/key";
 
 export default function App() {
+	const [user, setUser] = useState(null);
+
+	if (!firebase.apps.length) {
+		firebase.initializeApp(firebaseConfig);
+	}
+
+	firebase.auth().onAuthStateChanged((usr) => {
+		setUser(usr);
+	});
+
+	console.log(user);
+
 	return (
-		<View style={styles.container}>
-			<NavigationContainer>
-				<BottomTabs />
-			</NavigationContainer>
-		</View>
+		<SafeAreaView style={{ flex: 1 }}>
+			<View style={styles.container}>
+				<NavigationContainer>
+					{user ? <BottomTabs /> : <AuthStack />}
+				</NavigationContainer>
+			</View>
+		</SafeAreaView>
 	);
 }
 
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
-		marginTop: 30,
 	},
 });
