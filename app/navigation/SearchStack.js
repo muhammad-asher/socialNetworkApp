eed789a0b14e1e23e09651541d993765c18a88f9
muhammad-asher <70090506+muhammad-asher@users.noreply.github.com>
2021-04-15 16:51:48 +0500

    Update SearchStack.js

--- app/navigation/SearchStack.js
+++ app/navigation/SearchStack.js
@@ -1,9 +1,7 @@
 import React from "react";
-import { Image, StyleSheet } from "react-native";
+import { StyleSheet } from "react-native";
 import { createStackNavigator } from "@react-navigation/stack";
-import ProfileScreen from "../screens/ProfileScreen";
 import SearchScreen from "../screens/SearchScreen";
-import { camera, checklist, bell } from "../../assets/icons";
 
 const Stack = createStackNavigator();
 
